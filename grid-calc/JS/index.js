e6c955871fdcda9f494ec5a2d4d00ddf941fec2c
Jorgevvs <jorgevinicius9@gmail.com>
2021-03-08 09:49:00 -0300

    calc com soma

--- grid-calc/JS/index.js
+++ grid-calc/JS/index.js
@@ -6,38 +6,38 @@ function limpar(){
     display.innerHTML= numdisplay;
 }
 function click1(){
-    numdisplay += 1;
+    numdisplay += "1";
     display.innerHTML= numdisplay;
 }
 function click2(){
-    numdisplay += 2;
+    numdisplay += "2";
     display.innerHTML= numdisplay;
 }
 function click3(){
-    numdisplay += 3;
+    numdisplay += "3";
     display.innerHTML= numdisplay;
 }
 function click4(){
-    numdisplay += 4;
+    numdisplay += "4";
     display.innerHTML= numdisplay;
 }
 function click5(){
-    numdisplay += 5;
+    numdisplay += "5";
     display.innerHTML= numdisplay;
 }
 function click6(){
-    numdisplay += 6;
+    numdisplay += '6';
     display.innerHTML= numdisplay;
 }
 function click7(){
-    numdisplay += 7;
+    numdisplay += '7';
     display.innerHTML= numdisplay;
 }
 function click8(){
-    numdisplay += 8;
+    numdisplay += '8';
     display.innerHTML= numdisplay;
 }function click9(){
-    numdisplay += 9;
+    numdisplay += '9';
     display.innerHTML= numdisplay;
 }function click0(){
     numdisplay += 0;
@@ -45,9 +45,9 @@ function click8(){
 }
 function soma(){
     n1 = numdisplay;
-    numdisplay = 0;
+    numdisplay = '';
 }
 function show(){
-    numdisplay += parseInt(n1);
+    numdisplay = parseInt(numdisplay) + parseInt(n1);
     display.innerHTML= numdisplay;
 }
